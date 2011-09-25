073723171bad021373f1de128146afaa9f115f72
sphere <sphere@dersphere.de>
2011-09-25 11:55:31 +0200

    Added is_update option to add items which gets passed along to xbmcplugin.endOfDirectory(). Closes gh-3. Thanks dersphere.
    This is very useful for paginations. If you are on page 3 (which
    you entered through page 1 and 2) and you go one folder up (via "..")
    you will go to the real parent instead of the previous page 2.

--- lib/xbmcswift/plugin.py
+++ lib/xbmcswift/plugin.py
@@ -190,7 +190,8 @@ class Plugin(object):
         #return li
         return options['url'], li, options.get('is_folder', True)
 
-    def add_items(self, iterable):
+    def add_items(self, iterable, is_update=False):
+        # Set is_update to True if this is only an update and not a subfolder, useful for paginations
         # If we are in debug mode, do not make the call to xbmc
         # for each item
         #   if in debug mode, print it to command line
@@ -217,7 +218,7 @@ class Plugin(object):
         if self._mode is 'xbmc':
             if not xbmcplugin.addDirectoryItems(self.handle, items, len(items)):
                 raise Exception, 'problem?'
-            xbmcplugin.endOfDirectory(self.handle)
+            xbmcplugin.endOfDirectory(self.handle, updateListing=is_update)
 
         return urls
 
